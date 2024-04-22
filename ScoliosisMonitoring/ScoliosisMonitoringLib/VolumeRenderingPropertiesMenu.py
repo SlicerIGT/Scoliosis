d54393ed249202115aac1821840c88914721cabf
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2024-04-22 11:36:12 -0400

    COMP: Fix discovery of ScoliosisMonitoring module reorganizing sub-packages
    Prevent discovery of sub-packages as a regular module by introducing
    ScoliosisMonitoringLib.

--- ScoliosisMonitoring/ScoliosisMonitoringLib/VolumeRenderingPropertiesMenu.py
+++ ScoliosisMonitoring/ScoliosisMonitoringLib/VolumeRenderingPropertiesMenu.py
@@ -25,21 +25,20 @@ class VolumeRenderingPropertiesMenu(qt.QWidget):
         #self.checkBoxVisible2D.setTristate(False)
         self.checkBoxVisible3D = qt.QCheckBox()
         self.checkBoxVisible3D.setTristate(False)
-        
+
         #self.propertiesMenuWidget.setCellWidget(0,0,qt.QLabel("Visible2D"))
         #self.propertiesMenuWidget.setCellWidget(0,1,self.checkBoxVisible2D)
         self.visible3DFrame=qt.QFrame()
         self.visible3DFrame.setLayout(qt.QHBoxLayout())
         self.visible3DFrame.layout().addWidget(qt.QLabel("Visible in 3D"))
         self.visible3DFrame.layout().addWidget(self.checkBoxVisible3D)
-    
-        
+
         self.thresholdFrame=qt.QFrame()
         self.thresholdFrame.setLayout(qt.QHBoxLayout())
         self.thresholdLabel = qt.QLabel("Opacity Range:")
         self.thresholdLabel.setToolTip("Set the opacity range.")
         self.thresholdFrame.layout().addWidget(self.thresholdLabel)
-   
+
         self.threshold = ctk.ctkRangeWidget()
         self.threshold.spinBoxAlignment = 0xff # put enties on top
 
@@ -48,13 +47,13 @@ class VolumeRenderingPropertiesMenu(qt.QWidget):
         self.threshold.singleStep = 1.
         # set min/max based on current range
         self.thresholdFrame.layout().addWidget(self.threshold)
-        
+
         self.colorFrame=qt.QFrame()
         self.colorFrame.setLayout(qt.QHBoxLayout())
         self.colorLabel = qt.QLabel("Color Range:")
         self.colorLabel.setToolTip("Set the color range.")
         self.colorFrame.layout().addWidget(self.colorLabel)
-   
+
         self.colorRangeSlider = ctk.ctkRangeWidget()
         self.colorRangeSlider.spinBoxAlignment = 0xff # put enties on top
         self.colorRangeSlider.singleStep = 0.01
@@ -62,30 +61,26 @@ class VolumeRenderingPropertiesMenu(qt.QWidget):
         self.colorRangeSlider.maximum = 255.
         # set min/max based on current range
         self.colorFrame.layout().addWidget(self.colorRangeSlider)
-        
+
         #success, lo, hi = self.getBackgroundScalarRange()
         #if success:
         #  self.threshold.minimum, self.threshold.maximum = lo, hi
         #  self.threshold.singleStep = (hi - lo) / 1000.
-        
+
         #self.thresholdFrame.layout().addWidget(self.threshold)
 
         self.propertiesMenuWidget.layout().addWidget(self.visible3DFrame)
         self.propertiesMenuWidget.layout().addWidget(self.thresholdFrame )
         self.propertiesMenuWidget.layout().addWidget(self.colorFrame )
-       
-        
-        #self.timer = qt.QTimer()
 
+        #self.timer = qt.QTimer()
 
         #self.connections.append( (self.timer, 'timeout()', self.preview) )
         #self.connections.append( (self.threshold, 'valuesChanged(double,double)', self.onThresholdValuesChanged) )
-        
-        
+
     def show(self):
-        self.propertiesMenuWidget.show()    
-        
-        
+        self.propertiesMenuWidget.show()
+
     def contextMenuEvent(self, event):
          menu = qt.QMenu(self)
          quitAction = menu.addAction("Quit")
